444de988fd26c93b9a369788cb7a2bc819e96cb3
Matthew Blake <49418744+bmattblake@users.noreply.github.com>
2024-03-16 16:48:24 -0400

    added route for adding journal entries

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello World, from Flask</h1>"
+    return "<h1>Hello MindGardenAI, from Flask</h1>"
 
 @app.route("/login")
 def login():
@@ -13,6 +13,24 @@ def login():
 @app.route("/blingbling")
 def blingbling():
     return "<div>Yahoooooo</div>"
+
+
+
+@app.route("/add_entry", methods = ["POST"])
+def add_entry():
+    request_data = request.get_json()
+
+    id = request_data["id"]
+    title = request_data["title"]
+
+    out = f"{id} + {title}"
+    return out
+    
+        
+
+@app.route("/<usr>")
+def user(usr):
+    return usr
 
 if __name__ == "__main__":
     app.run(debug=True)
