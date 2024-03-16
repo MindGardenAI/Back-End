1ad1ea85b6d05b0daf6d2e696d184336b23d5eed
Matthew Blake <49418744+bmattblake@users.noreply.github.com>
2024-03-16 15:23:23 -0400

    added jacob's modifications

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World, from Flask"
+    return "<h1>Hello World, from Flask</h1>"
+
+@app.route("/login")
+def login():
+    return "<h1>Login here</h1>"
+
+@app.route("/blingbling")
+def blingbling():
+    return "<div>Yahoooooo</div>"
 
 if __name__ == "__main__":
     app.run(debug=True)
