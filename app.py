3b74ef89eed7063255cc61a36729f653277c36be
jacobpiehl <57500410+jacobpiehl@users.noreply.github.com>
2024-03-17 16:25:50 +0000

    Revert "Adjustments for routing to home"
    This reverts commit 1f6ed8de1e477cc1779d312801de1a616046d0c8.

--- app.py
+++ app.py
@@ -48,6 +48,39 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
 )
 
+@app.route("/api/public")
+def public():
+    """No access token required."""
+    response = (
+        "Hello from a public endpoint! You don't need to be"
+        " authenticated to see this."
+    )
+    return jsonify(message=response)
+
+
+# @app.route("/api/private")
+# @require_auth(None)
+# def private():
+#     """A valid access token is required."""
+#     response = (
+#         "Hello from a private endpoint! You need to be"
+#         " authenticated to see this."
+#     )
+#     return jsonify(message=response)
+
+
+# @app.route("/api/private-scoped")
+# @require_auth("read:messages")
+# def private_scoped():
+#     """A valid access token and scope are required."""
+#     response = (
+#         "Hello from a private endpoint! You need to be"
+#         " authenticated and have a scope of read:messages to see"
+#         " this."
+#     )
+#     return jsonify(message=response)
+
+
 
 @app.route("/")
 def home():
